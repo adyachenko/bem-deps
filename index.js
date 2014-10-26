5109ac00c76ce2e769e4b4fd6ccd2b678d467d5c
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-10-26 15:24:04 +0500

    WIP: add basic required/expected resolution

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-function Deps() {
-    return {
-        deps: function () {
+var rod = require('require-or-die');
+var object = require('bem-object');
+var glue = require('glue-streams');
+var join = require('path').join;
+var through = require('through2');
 
-        }
-    };
+function BemDeps(levels, options) {
+
+    if (!(this instanceof BemDeps)) {
+        return new BemDeps(levels, options);
+    }
+
+    options = options || {};
+    this.levels = levels;
+    this.normalize = options.normalize;
 }
 
-module.exports = Deps;
+BemDeps.prototype.deps = function deps(path) {
+    var bem = new object(path, this.options);
+    var output = through.obj();
+
+    // TODO: block could be in upper levels
+    // TODO: deps.js file could be not in strict directory
+    rod(join(bem.level, bem.id + '.deps.js'), function (err, value) {
+        if (err) { return output.emit('error', err); }
+
+        var required = this.normalize(value.require || value.mustDeps).map(deps, this);
+        var expected = this.normalize(value.expect || value.shouldDeps).map(deps, this);
+
+        glue(required, bem, expected).pipe(output);
+    });
+
+    return output;
+};
+
+module.exports = BemDeps;
