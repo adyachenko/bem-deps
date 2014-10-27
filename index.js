40b63b211d8b254ccd2f50a00677b6723ae91e07
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-10-27 15:44:14 +0500

    Switch to sync implementation

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var rod = require('require-or-die');
 var object = require('bem-object');
-var glue = require('glue-streams');
 var join = require('path').join;
 var resolve = require('path').resolve;
-var through = require('through2');
 var normalize = require('deps-normalize');
+var fs = require('fs');
+var flatit = require('flatit');
+var array = require('stream-array');
 
 function BemDeps(levels, options) {
     if (!(this instanceof BemDeps)) {
@@ -31,6 +32,7 @@ BemDeps.prototype._parentLevels = function _parentLevels(bem) {
 BemDeps.prototype.required = function required(value, level) {
     var self = this;
     value.require = value.require || value.mustDeps || [];
+    if (!Array.isArray(value.require)) { value.require = [value.require]; }
     return self.normalize(value.require.map(function (req) {
         if (typeof req === 'string') { req = new object(req, self.options); }
         req.level = level;
@@ -41,6 +43,7 @@ BemDeps.prototype.required = function required(value, level) {
 BemDeps.prototype.expected = function expected(value, level) {
     var self = this;
     value.expect = value.expect || value.shouldDeps || [];
+    if (!Array.isArray(value.expect)) { value.expect = [value.expect]; }
     return self.normalize(value.expect.map(function (exp) {
         if (typeof exp === 'string') { exp = new object(exp, self.options); }
         exp.level = level;
@@ -48,7 +51,15 @@ BemDeps.prototype.expected = function expected(value, level) {
     }));
 };
 
-BemDeps.prototype.deps = function deps(path, options) {
+function setParent(parent) {
+    return function (bem) {
+        bem.parent = parent;
+        parent.copy(bem);
+        return bem;
+    };
+}
+
+BemDeps.prototype._deps = function _deps(path, options) {
     var self = this;
 
     options = options || {};
@@ -61,35 +72,44 @@ BemDeps.prototype.deps = function deps(path, options) {
     var bem = new object(path, self.options);
     var parents = this._parentLevels(bem);
 
-    var streams = {
-        required: [],
-        blocks: [],
-        expected: []
-    };
+    var required = [];
+    var blocks = [];
+    var expected = [];
 
     parents.map(function (level) {
-        var required = through.obj();
-        streams.required.push(required);
+        // Temporary hack to filter out non-existent entities
+        if (!fs.existsSync(join(level, bem.block))) {
+            return;
+        }
 
-        var expected = through.obj();
-        streams.expected.push(expected);
+        var parent = new object(join(level, bem.id));
+        blocks.push(parent);
 
-        streams.blocks.push(new object(join(level, bem.id)));
+        var depsFile = join(level, bem.block, bem.id + '.deps.js');
 
-        rod(join(level, bem.block, bem.id + '.deps.js'), function (err, value) {
-            if (err) {
-                // No deps.js file -> no dependencies
-                required.end();
-                expected.end();
-                return;
-            }
+        if (!fs.existsSync(depsFile)) {
+            return;
+        }
 
-            glue.obj.apply(glue, self.required(value, level).map(self.deps, self)).pipe(required);
-            glue.obj.apply(glue, self.expected(value, level).map(self.deps, self)).pipe(expected);
-        });
+        var value = rod.sync(depsFile);
+        required = required.concat(
+            self.required(value, level)
+                .map(setParent(parent))
+                .map(self._deps, self)
+        );
+
+        expected = expected.concat(
+            self.expected(value, level)
+                .map(setParent(parent))
+                .map(self._deps, self)
+        );
     });
 
-    return glue.obj.apply(glue, streams.required.concat(streams.blocks).concat(streams.expected));
+    return flatit([required, blocks, expected]);
+};
+
+BemDeps.prototype.deps = function deps(path, options) {
+    return array(this._deps(path, options));
 };
 
 module.exports = BemDeps;
